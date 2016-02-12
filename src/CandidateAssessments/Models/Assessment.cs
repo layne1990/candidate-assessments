c4d06ac6cae0b0fb9d7edf6266c5afa3d9385aae
Patrick Jenkins <patrick.jenkins213@gmail.com>
2016-02-12 15:58:28 -0500

    End of day commit

--- src/CandidateAssessments/Models/Assessment.cs
+++ src/CandidateAssessments/Models/Assessment.cs
@@ -13,10 +13,18 @@ namespace CandidateAssessments.Models
         [MaxLength(128)]
         public string CandidateName { get; set; }
 
+        [Required]
         [MaxLength(36)]
         public string AccessCode { get; set; }
 
+        // Probably don't need this since we can just grab the time when the user hits the create button
+        [Required]
+        [DataType(DataType.DateTime)]
         public DateTime CreatedDate { get; set; }
+
+        // Could probably also preset this to be a week from the day the assessment is created
+        [Required]
+        [DataType(DataType.DateTime)]
         public DateTime ExpirationDate { get; set; }
 
         public List<Quiz> Quizes { get; set; }
