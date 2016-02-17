543879c2ce2d5c0b95692c8b016a123c01534006
Patrick Jenkins <patrick.jenkins213@gmail.com>
2016-02-17 11:34:44 -0500

    Trying to add checkboxes

--- src/CandidateAssessments/Models/Quiz.cs
+++ src/CandidateAssessments/Models/Quiz.cs
@@ -7,20 +7,20 @@ namespace CandidateAssessments.Models
 {
     public class Quiz
     {
+
         public int QuizId { get; set; }
-        public int TopicId { get; set; }
 
         public Topic Topic{get;set;}
 
-        public int AssessmentId { get; set; }
         public Assessment Assessment { get; set; }
+
+        // this can be calculated based on the length of this.Questions, do we need to keep it around?
         public int NumberOfQuestions { get; set; }
         public int NumberCorrect { get; set; }
         public int TimeLimit { get; set; }
         
         public DateTime TimeStarted { get; set; }
         public DateTime TimeCompleted { get; set; }
-
 
         public List<QuizQuestion> Questions { get; set; }
     }
