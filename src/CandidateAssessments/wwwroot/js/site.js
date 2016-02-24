642329aef5b2bbd6a62a6dbc55ea2a8bfe74a3ec
Nick Peltzer <peltzer@rdacorp.com>
2016-02-24 14:08:12 -0500

    Fixed edit for different question types

--- src/CandidateAssessments/wwwroot/js/site.js
+++ src/CandidateAssessments/wwwroot/js/site.js
@@ -14,20 +14,7 @@
         }
     })
 });
-function changeQtype() {
-    var select = $("#QuestionType option:selected").val();
 
-    $("#offForTrueFalse").show();
-    $("#offForFillInBlank").show();
-    $("#onTrueFalse").hide();
-    if (select == "FillInBlank") {
-        $("#offForFillInBlank").hide();
-    } else if (select == "TrueFalse") {
-        $("#onTrueFalse").show();
-        $("#offForTrueFalse").hide();
-
-    }
-}
 $(function () {
     $("#fillin").on('keyup', function () {
         $('#AnswerSubmit').removeClass('hidden');
