0fdf584f62c0716275d7c40859a54e7996c59cc9
Nick Peltzer <peltzer@rdacorp.com>
2016-03-10 09:16:18 -0500

    Made difficulty searchable in topic questions

--- src/CandidateAssessments/Controllers/TopicQuestionsController.cs
+++ src/CandidateAssessments/Controllers/TopicQuestionsController.cs
@@ -41,7 +41,7 @@ namespace WebApplication1.Controllers
             if (searchParam != null)
             {
                 assessmentContext = assessmentContext.Where(x => x.QuestionText.ToLower().Contains(searchParam.ToLower()) || x.TopicQuestionId.ToString().Contains(searchParam.ToLower()) || x.ChoiceA.ToLower().Contains(searchParam.ToLower()) ||
-                x.ChoiceB.ToLower().Contains(searchParam.ToLower()) || x.ChoiceC.ToLower().Contains(searchParam.ToLower()) || x.ChoiceD.ToLower().Contains(searchParam.ToLower()) ||
+                x.ChoiceB.ToLower().Contains(searchParam.ToLower()) || x.ChoiceC.ToLower().Contains(searchParam.ToLower()) || x.DifficultyLevel.ToString().ToLower().Contains(searchParam.ToLower()) || x.ChoiceD.ToLower().Contains(searchParam.ToLower()) ||
                x.Topic.Name.ToLower().Contains(searchParam.ToLower())).ToList();
             }
 
